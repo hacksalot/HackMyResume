c54137a4935849518f215dc46cd2f213bca05e64
devlinjd <james@indevious.com>
2015-09-10 09:13:12 -0400

    Cleanup.

--- src/sheet.js
+++ src/sheet.js
@@ -1,21 +1,26 @@
+/**
+Abstract character/resume sheet representation.
+@license Copyright (c) 2015 by James M. Devlin. All rights reserved.
+*/
+
 (function() {
 
-var FS = require('fs');
+  var FS = require('fs');
 
-function Sheet() {
+  function Sheet() {
+    this.id = null;
+    this.title = "New";
+    this.rep = { };
+    this.fileName = null;
+  }
 
-  this.id = null;
-  this.title = "";
-  this.sheets = [];
-  this.rep = { };
+  Sheet.prototype.open = function( file, title ) {
+    this.rep = JSON.parse( FS.readFileSync( file, 'utf8' ) );
+    this.fileName = file;
+    this.title = title || this.rep.basics.name;
+    return this;
+  };
 
-}
-
-Sheet.prototype.open = function( file ) {
-  this.rep = JSON.parse( FS.readFileSync( file, 'utf8' ) );
-  return this;
-};
-
-module.exports = Sheet;
+  module.exports = Sheet;
 
 }());
