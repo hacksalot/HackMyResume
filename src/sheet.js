ffa06118a0a8c62333156aabef9a5820a29e477f
devlinjd <james@indevious.com>
2015-09-17 07:27:30 -0400

    Minor extensions to Sheet class.

--- src/sheet.js
+++ src/sheet.js
@@ -15,6 +15,9 @@ Abstract character/resume sheet representation.
     this.fileName = null;
   }
 
+  /**
+  Open and parse the specified JSON resume sheet.
+  */
   Sheet.prototype.open = function( file, title ) {
     var rep = JSON.parse( FS.readFileSync( file, 'utf8' ) );
     extend( true, this, rep );
@@ -23,6 +26,16 @@ Abstract character/resume sheet representation.
     this.title = title || this.basics.name;
     return this;
   };
+
+  /**
+  Determine if the sheet includes a specific social profile (eg, GitHub).
+  */
+  Sheet.prototype.hasProfile = function( socialNetwork ) {
+    socialNetwork = socialNetwork.trim().toLowerCase();
+    return this.basics.profiles && this.basics.profiles.filter(function(prof) {
+      return prof.network.trim().toLowerCase() === socialNetwork;
+    }).length > 0;
+  }
 
   module.exports = Sheet;
 
