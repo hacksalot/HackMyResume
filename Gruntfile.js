bf34b01367985c96893d11a030abd46a66b97b70
devlinjd <james@indevious.com>
2015-11-21 03:10:11 -0500

    Add YUIDoc support

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,15 +15,33 @@ module.exports = function (grunt) {
         reporter: 'spec'
       },
       all: { src: ['tests/*.js'] }
+    },
+
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: 'src/',
+          //themedir: 'path/to/custom/theme/',
+          outdir: 'docs/'
+        }
+      }
     }
 
   };
 
   grunt.initConfig( opts );
   grunt.loadNpmTasks('grunt-simple-mocha');
-  grunt.registerTask('test', 'Test the FluentLib library.', function( config ) {
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+  grunt.registerTask('test', 'Test the FluentCV library.', function( config ) {
     grunt.task.run( ['simplemocha:all'] );
   });
-  grunt.registerTask('default', [ 'test' ]);
+  grunt.registerTask('document', 'Generate FluentCV library documentation.', function( config ) {
+    grunt.task.run( ['yuidoc'] );
+  });
+  grunt.registerTask('default', [ 'test', 'yuidoc' ]);
 
 };
