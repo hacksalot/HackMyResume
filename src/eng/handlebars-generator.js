d708a6c6d8d482fc4cc22b9d044bf55ff3ab527f
hacksalot <hacksalot@fluentdesk.com>
2015-12-18 10:10:30 -0500

    Refactor Handlebars helpers.

--- src/eng/handlebars-generator.js
+++ src/eng/handlebars-generator.js
@@ -11,9 +11,8 @@ Definition of the HandlebarsGenerator class.
   var _ = require('underscore')
     , HANDLEBARS = require('handlebars')
     , FS = require('fs')
-    , moment = require('moment')
-    , MD = require('marked')
-    , H2W = require('../utils/html-to-wpml');
+    , _ = require('underscore')
+    , registerHelpers = require('./handlebars-helpers');
 
 
 
@@ -43,60 +42,6 @@ Definition of the HandlebarsGenerator class.
     });
 
   };
-
-
-
-  /**
-  Register useful Handlebars helpers.
-  @method registerHelpers
-  */
-  function registerHelpers() {
-
-    // Set up a date formatting helper so we can do:
-    // {{#formatDate val 'YYYY-MM'}}
-    HANDLEBARS.registerHelper("formatDate", function(datetime, format) {
-      if( moment ) {
-        return moment( datetime ).format( format );
-      }
-      else {
-        return datetime;
-      }
-    });
-
-    // Set up a Markdown-to-WordProcessingML helper so we can do:
-    // {{#wmpl val [true|false]}}
-    HANDLEBARS.registerHelper("wpml", function( txt, inline ) {
-      inline = (inline && !inline.hash) || false;
-      txt = inline ?
-        MD(txt.trim()).replace(/^\s*<p>|<\/p>\s*$/gi, '') :
-        MD(txt.trim());
-      txt = H2W( txt.trim() );
-      return txt;
-    });
-
-    // Set up a generic conditional helper so we can do:
-    // {{#compare val otherVal operator="<"}}
-    // http://doginthehat.com.au/2012/02/comparison-block-helper-for-handlebars-templates/
-    HANDLEBARS.registerHelper('compare', function(lvalue, rvalue, options) {
-      if (arguments.length < 3)
-        throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
-      var operator = options.hash.operator || "==";
-      var operators = {
-          '==':       function(l,r) { return l == r; },
-          '===':      function(l,r) { return l === r; },
-          '!=':       function(l,r) { return l != r; },
-          '<':        function(l,r) { return l < r; },
-          '>':        function(l,r) { return l > r; },
-          '<=':       function(l,r) { return l <= r; },
-          '>=':       function(l,r) { return l >= r; },
-          'typeof':   function(l,r) { return typeof l == r; }
-      };
-      if (!operators[operator])
-        throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
-      var result = operators[operator](lvalue,rvalue);
-      return result ? options.fn(this) : options.inverse(this);
-    });
-  }
 
 
 
