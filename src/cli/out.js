f18910f490d06d887a6e32ff365f4b4ee711fa78
hacksalot <hacksalot@indevious.com>
2016-01-09 20:18:56 -0500

    Generate ANALYZE console output from Handlebars template.

--- src/cli/out.js
+++ src/cli/out.js
@@ -15,6 +15,9 @@ Output routines for HackMyResume.
     , _ = require('underscore')
     , Class = require('../utils/class.js')
     , PATH = require('path')
+    , LO = require('lodash')
+    , FS = require('fs')
+    , HANDLEBARS = require('handlebars')
     , pad = require('string-padding');
 
 
@@ -128,45 +131,18 @@ Output routines for HackMyResume.
           break;
 
         case HME.afterAnalyze:
-          // TODO: templatize all of this
           var info = evt.info;
-          var padding = 20;
-          this.log(chalk.cyan.bold('\nSECTIONS') + chalk.cyan(' (') +
-            chalk.white.bold(_.keys(info.totals).length) + chalk.cyan('):\n'));
-
-          _.each( info.totals, function(tot, key) {
-            this.log(chalk.cyan(pad(key + ': ',20)) +
-            chalk.cyan.bold(pad(tot.toString(),5)));
-          }, this);
-
-          this.log();
-          this.log(chalk.cyan.bold('COVERAGE') + chalk.cyan(' (') + chalk.white.bold( info.coverage.pct ) + chalk.cyan('):\n'));
-          this.log(chalk.cyan(pad('Total Days: ', padding)) + chalk.cyan.bold( pad(info.coverage.duration.total.toString(),5) ));
-          this.log(chalk.cyan(pad('Employed: ', padding)) + chalk.cyan.bold( pad((info.coverage.duration.total - info.coverage.duration.gaps).toString(),5) ));
-          this.log(chalk.cyan(pad('Gaps:     ', padding + 4)) + chalk.cyan.bold(info.coverage.gaps.length) + chalk.cyan('  [') + info.coverage.gaps.map(function(g) {
-              var clr = 'green';
-              if( g.duration > 35 ) clr = 'yellow';
-              if( g.duration > 90 ) clr = 'red';
-              return chalk[clr].bold( g.duration) ;
-            }).join(', ') + chalk.cyan(']') );
-
-          this.log(chalk.cyan(pad('Overlaps:     ', padding + 4)) + chalk.cyan.bold(info.coverage.overlaps.length) + chalk.cyan('  [') + info.coverage.overlaps.map(function(ol) {
-              var clr = 'green';
-              if( ol.duration > 35 ) clr = 'yellow';
-              if( ol.duration > 90 ) clr = 'red';
-              return chalk[clr].bold( ol.duration) ;
-            }).join(', ') + chalk.cyan(']') );
-
+          var rawTpl = FS.readFileSync( PATH.join( __dirname, 'analyze.hbs' ), 'utf8');
+          HANDLEBARS.registerHelper( consoleFormatHelpers );
+          var template = HANDLEBARS.compile(rawTpl, { strict: false, assumeObjects: false });
           var tot = 0;
-          this.log();
-          this.log( chalk.cyan.bold('KEYWORDS') + chalk.cyan(' (') + chalk.white.bold( info.keywords.length ) +
-            chalk.cyan('):\n\n') +
-            info.keywords.map(function(g) {
-              tot += g.count;
-              return chalk.cyan( pad(g.name + ': ', padding) ) + chalk.cyan.bold( pad( g.count.toString(), 5 )) + chalk.cyan(' mentions');
-            }).join('\n'));
+          info.keywords.forEach(function(g) {
+            tot += g.count;
+          });
+          info.keywords.totalKeywords = tot;
+          var output = template( info );
 
-          this.log(chalk.cyan( pad('TOTAL: ', padding) ) + chalk.white.bold( pad( tot.toString(), 5 )) + chalk.cyan(' mentions'));
+          this.log(chalk.cyan(output));
           break;
 
         case HME.beforeConvert:
@@ -182,6 +158,51 @@ Output routines for HackMyResume.
 
   });
 
+
+  var consoleFormatHelpers = {
+    v: function( val, defaultVal, padding, style ) {
+      retVal = ( val === null || val === undefined ) ? defaultVal : val;
+      var spaces = 0;
+      if( String.is(padding) ) {
+        spaces = parseInt( padding, 10 );
+        if( isNaN(spaces) ) spaces = 0;
+      }
+      else if( _.isNumber(padding) ) {
+        spaces = padding;
+      }
+
+      if( spaces !== 0 )
+        retVal = pad( retVal, Math.abs(spaces), null, spaces > 0 ? pad.LEFT : pad.RIGHT );
+
+      if( style && String.is( style )) {
+        retVal = LO.get( chalk, style )( retVal );
+      }
+
+      return retVal;
+    },
+
+    heat: function(val) {
+      if( val < 35 )
+        return chalk.green.bold(val);
+      else if( val < 95 )
+        return chalk.yellow.bold(val);
+      else
+        return chalk.red.bold(val);
+    },
+
+    style: function( val, style ) {
+      return LO.get( chalk, style )( val );
+    },
+
+    isPlural: function( val, options ) {
+      if( val > 1 )
+        return options.fn(this);
+    },
+
+    pad: function( val, spaces ) {
+      return pad(val, Math.abs(spaces), null, spaces > 0 ? pad.LEFT : pad.RIGHT );
+    }
+  };
 
 
 }());
--- src/inspectors/totals-inspector.js
+++ src/inspectors/totals-inspector.js
@@ -30,25 +30,29 @@ Section analysis for HackMyResume.
     /**
     Run the Totals Inspector on a resume.
     @method run
-    @return An array of objects containing summary information for each section
-    on the resume.
+    @return An object containing summary information for each section on the
+    resume.
     */
     run: function( rez ) {
 
-      var ret = { };
+      var sectionTotals = { };
       _.each( rez, function(val, key){
         if( _.isArray( val ) && !_.isString(val) ) {
-          ret[ key ] = val.length;
+          sectionTotals[ key ] = val.length;
         }
         else if( val.history && _.isArray( val.history ) ) {
-          ret[ key ] = val.history.length;
+          sectionTotals[ key ] = val.history.length;
         }
         else if( val.sets && _.isArray( val.sets ) ) {
-          ret[ key ] = val.sets.length;
+          sectionTotals[ key ] = val.sets.length;
         }
       });
 
-      return ret;
+      return {
+        totals: sectionTotals,
+        numSections: Object.keys( sectionTotals ).length
+      };
+
     }
 
 
