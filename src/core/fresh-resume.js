6cd1e60e79f2751b6a7b0e4f7c9e1cb661fc9a8b
hacksalot <hacksalot@indevious.com>
2016-01-12 12:46:18 -0500

    Sort projects.

--- src/core/fresh-resume.js
+++ src/core/fresh-resume.js
@@ -462,7 +462,7 @@ Definition of the FRESHResume class.
     sortSection( 'employment.history' );
     sortSection( 'education.history' );
     sortSection( 'service.history' );
-    sortSection( 'projects.history' );
+    sortSection( 'projects' );
 
     // this.awards && this.awards.sort( function(a, b) {
     //   return( a.safeDate.isBefore(b.safeDate) ) ? 1
