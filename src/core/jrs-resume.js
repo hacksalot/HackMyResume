e9fb9b37d1e282999bf82f8717efb9bbfa4a3e01
Sykomaniac <ashleysykes01@gmail.com>
2019-08-01 16:40:45 +0100

    Implement same fix for project description
    - Also fix for project description

--- src/core/jrs-resume.js
+++ src/core/jrs-resume.js
@@ -281,7 +281,7 @@ class JRSResume {
     return transformer(ret,
       [ 'skills','url','website','startDate','endDate', 'releaseDate', 'date',
       'phone','email','address','postalCode','city','country','region',
-      'safeStartDate','safeEndDate', 'summary' ],
+      'safeStartDate','safeEndDate', 'summary', 'description' ],
       (key, val) => HD(val));
   }
 }
